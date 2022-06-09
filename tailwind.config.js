693153ed8badd958b3c0d2488a8bf9fcec03e44e
Zaie Hilmi <zaiehilmi@gmail.com>
2022-06-09 14:59:06 +0800

    add tailwind component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
