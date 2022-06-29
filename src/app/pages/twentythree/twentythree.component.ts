4e14214a1fadceb4e1b2e1120dda1c021787eac3
Zaie Hilmi <zaiehilmi@gmail.com>
2022-06-29 09:13:37 +0800

    - make map as a new component - some tailwind property converted to standard css

--- src/app/pages/twentythree/twentythree.component.ts
+++ src/app/pages/twentythree/twentythree.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { ToggleService } from 'src/app/toggle.service';
 
-declare const L: any;
-
 @Component({
   selector: 'app-twentythree',
   templateUrl: './twentythree.component.html',
   styleUrls: ['./twentythree.component.css'],
   providers: [ToggleService],
 })
-export class TwentythreeComponent implements OnInit {
+export class TwentythreeComponent {
 
   constructor(public dropdown: ToggleService) { }
 
@@ -20,38 +18,5 @@ export class TwentythreeComponent implements OnInit {
     this.dropdown.toggleDropdown();
   }
 
-  ngOnInit() {
-    if (!navigator.geolocation) {
-      console.log('location is not supported');
-    }
-    navigator.geolocation.getCurrentPosition(position => {
-      const coords = position.coords;
 
-      let map = L.map('map').setView([coords.latitude, coords.longitude], 13);
-      console.log(map)
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-      }).addTo(map);
-
-      let marker = L.marker([coords.latitude, coords.longitude]).addTo(map);
-      marker.bindPopup('<b>You are here</b><br>lat: ' + coords.latitude + '<br>lng: ' + coords.longitude).openPopup();
-    });
-
-    this.watchPosition();
-  }
-
-  watchPosition() {
-    let desLat = 0;
-
-    let currentLocation = navigator.geolocation.watchPosition(position => {
-      console.log(`lat: ${position.coords.latitude} lng: ${position.coords.longitude}`);
-
-      if (position.coords.latitude === desLat) navigator.geolocation.clearWatch(currentLocation);
-    }, (err) => { console.log(err); }, {
-      enableHighAccuracy: false,
-      timeout: 5000,
-      maximumAge: 0,
-    });
-  }
 }
