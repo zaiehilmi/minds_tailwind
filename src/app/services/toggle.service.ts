532dee3add482086e9285df2772caf909ad8d30c
Zaie Hilmi <zaiehilmi@gmail.com>
2022-07-06 10:46:17 +0800

    refactor to allow multiple toggles usage

--- src/app/services/toggle.service.ts
+++ src/app/services/toggle.service.ts
@@ -4,14 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ToggleService {
-  showDropdown = false;
+  private toggle: iToggle[] = [];
 
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
+  register(id: string) {
+    this.toggle.push({
+      id: id,
+      visible: false
+    });
   }
 
-  isDropdownShown() {
-    return this.showDropdown;
+  isToggleShown(id: string): boolean {
+    return Boolean(this.toggle.find(t => t.id === id)?.visible);
   }
 
+  toggleComponent(id: string) {
+    const _toggle = this.toggle.find(t => t.id === id);
+
+    if (_toggle) _toggle.visible = !_toggle.visible;
+  }
+
+
+}
+
+interface iToggle {
+  id: string;
+  visible: boolean;
 }
