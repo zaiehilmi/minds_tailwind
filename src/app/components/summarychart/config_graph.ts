2d63ccfd9201453f9c9d80d4a78cf39eb121b9be
Zaie Hilmi <zaiehilmi@gmail.com>
2022-07-25 17:15:42 +0800

    some setting to config the graph visual

---
+++ src/app/components/summarychart/config_graph.ts
@@ -0,0 +1,9 @@
+export const ConfigGraph = {
+    colorBar: {
+        totalSubs: '#26AAFD',
+        totalCTT: '#82F8C3',
+        subscriber: '#5FD9E9',
+        CTT: '#FD77B4',
+    },
+    list_month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+}
